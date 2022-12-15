0f14da55d2d890f44da45c7b77988a86b0a36ef8
Todor Imreorov <blurymind@gmail.com>
2022-12-15 13:02:41 +0000

    setup dev and preview server

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 // main.ts
 const hello = (name: string) => {
+    const app =document.getElementById("app");
+    if(app){
+        app.innerHTML = "aerr TEST " + name
+    }
+
     return `Hello Todor, ${name}!`;
 };
 
